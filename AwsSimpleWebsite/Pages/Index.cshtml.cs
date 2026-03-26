92e99473ac6bb6ec91839e4ec76685091a3bbcba
Harshadbhai <tybca3113@gmail.com>
2026-03-26 14:38:30 +0530

    clear structure website  + api

---
+++ AwsSimpleWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AwsSimpleWebsite.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
